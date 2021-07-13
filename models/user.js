534be8377536ac168d16c8f9736ac7ed366d1f5f
Jason Godesky <jefgodesky@gmail.com>
2021-07-13 18:26:14 -0400

    Add discordID to User Schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const { Schema, model } = require('mongoose')
 
 const UserSchema = new Schema({
-  googleID: String
+  googleID: String,
+  discordID: String
 })
 
 module.exports = model('User', UserSchema)
