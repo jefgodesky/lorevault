3c6dd704d777803d64387a7bc8265dacec2d30f1
Jason Godesky <jefgodesky@gmail.com>
2021-07-13 18:28:00 -0400

    Add Discord login routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,18 @@ const passport = require('passport')
 const router = Router()
 
 // GET /login/google
-router.get('/login/google', passport.authenticate('google', { scope: ['profile','email'] }))
+router.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+
+// GET /login/discord
+router.get('/login/discord', passport.authenticate('discord', { scope: ['identify'] }))
 
 // GET /login/google/callback
 router.get('/login/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+  res.redirect('/')
+})
+
+// GET /login/discord/callback
+router.get('/login/discord/callback', passport.authenticate('discord', { failureRedirect: '/login' }), (req, res) => {
   res.redirect('/')
 })
 
