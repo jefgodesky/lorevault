1da4b98fa67792fe73a3aae2b7644318db599a95
Jason Godesky <jefgodesky@gmail.com>
2021-07-14 17:14:27 -0400

    Add characters to user schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,13 @@
 const { Schema, model } = require('mongoose')
 
+const CharacterSchema = new Schema({
+  name: String
+})
+
 const UserSchema = new Schema({
   googleID: String,
-  discordID: String
+  discordID: String,
+  characters: [CharacterSchema]
 })
 
 module.exports = model('User', UserSchema)
