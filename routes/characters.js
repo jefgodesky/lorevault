22974534656df192745091190551db7adfc53d45
Jason Godesky <jefgodesky@gmail.com>
2021-07-14 17:40:45 -0400

    Add new character

--- routes/characters.js
+++ routes/characters.js
@@ -6,4 +6,11 @@ router.get('/', (req, res, next) => {
   res.render('characters/index', req.viewOpts)
 })
 
+// POST /
+router.post('/', async (req, res, next) => {
+  req.user.characters.push({ name: req.body['new-char-name'] })
+  await req.user.save()
+  res.redirect('/characters')
+})
+
 module.exports = router
