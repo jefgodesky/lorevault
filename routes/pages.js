7740d2da1cb93d782390ec145dd2bc91baecf5a9
Jason Godesky <jefgodesky@gmail.com>
2021-07-14 20:31:08 -0400

    POST /create

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,27 @@
 const { Router } = require('express')
+const Page = require('../models/page')
 const router = Router()
 
 // GET /create
 router.get('/create', async (req, res, next) => {
   req.viewOpts.msg = 'Initial text'
   res.render('create', req.viewOpts)
+})
+
+// POST /create
+router.post('/create', async (req, res, next) => {
+  const data = {
+    title: req.body.title,
+    body: req.body.body
+  }
+  data.versions = [
+    Object.assign({}, data, {
+      msg: req.body.msg,
+      editor: req.user?._id
+    })
+  ]
+  const page = await Page.create(data)
+  res.redirect(`/${page.path}`)
 })
 
 module.exports = router
