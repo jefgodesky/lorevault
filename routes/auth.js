167f150f4bc297c5db1940a7b4f3bc517caea74b
Jason Godesky <jefgodesky@gmail.com>
2021-07-16 18:48:35 -0400

    GET /profile
    Replacing GET /characters, because there are other things that a user will need to take care of here, too.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const passport = require('passport')
+const { rules } = require('../config')
 const router = Router()
 
 // GET /login/google
@@ -16,6 +17,23 @@ router.get('/login/google/callback', passport.authenticate('google', { failureRe
 // GET /login/discord/callback
 router.get('/login/discord/callback', passport.authenticate('discord', { failureRedirect: '/login' }), (req, res) => {
   res.redirect('/')
+})
+
+// GET /profile
+router.get('/profile', async (req, res, next) => {
+  req.viewOpts.chars = req.user.characters
+  req.viewOpts.charSheet = []
+  for (const system of rules) {
+    const sheet = require(`../rules/${system}/sheet`)
+    for (const field of Object.keys(sheet)) {
+      req.viewOpts.charSheet.push({
+        id: `${system}-${field}`,
+        label: sheet[field].label,
+        type: sheet[field].type === Number ? 'number' : 'text'
+      })
+    }
+  }
+  res.render('profile', req.viewOpts)
 })
 
 // GET /logout
--- routes/characters.js
+++ routes/characters.js
@@ -2,23 +2,6 @@ const { Router } = require('express')
 const { rules } = require('../config')
 const router = Router()
 
-// GET /
-router.get('/', (req, res, next) => {
-  req.viewOpts.chars = req.user.characters
-  req.viewOpts.charSheet = []
-  for (const system of rules) {
-    const sheet = require(`../rules/${system}/sheet`)
-    for (const field of Object.keys(sheet)) {
-      req.viewOpts.charSheet.push({
-        id: `${system}-${field}`,
-        label: sheet[field].label,
-        type: sheet[field].type === Number ? 'number' : 'text'
-      })
-    }
-  }
-  res.render('characters/index', req.viewOpts)
-})
-
 // POST /
 router.post('/', async (req, res, next) => {
   const char = { name: req.body['new-char-name'] }
