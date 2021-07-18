09b95e2602918424e45450a7fed390bad06da33b
Jason Godesky <jefgodesky@gmail.com>
2021-07-18 17:47:59 -0400

    UserSchema.methods.disconnect — Add unit tests

--- models/user.spec.js
+++ models/user.spec.js
@@ -10,13 +10,34 @@ afterAll(async () => await db.close())
 describe('User', () => {
   describe('UserSchema.methods.isLastConnection', () => {
     it('returns false if you have at least one other connection', async () => {
+      expect.assertions(1)
       const user = await User.create({ googleID: 'google', discordID: 'discord' })
       expect(user.isLastConnection('google')).toEqual(false)
     })
 
     it('returns true if this is your only connection', async () => {
+      expect.assertions(1)
       const user = await User.create({ googleID: 'google' })
       expect(user.isLastConnection('google')).toEqual(true)
+    })
+  })
+
+  describe('UserSchema.methods.disconnect', () => {
+    it('won\'t remove your last connection', async () => {
+      expect.assertions(2)
+      const user = await User.create({ googleID: 'google' })
+      const actual = await user.disconnect('google')
+      expect(actual).toEqual(false)
+      expect(user.googleID).toEqual('google')
+    })
+
+    it('will remove a connection if you have another', async () => {
+      expect.assertions(3)
+      const user = await User.create({ googleID: 'google', discordID: 'discord' })
+      const actual = await user.disconnect('google')
+      expect(actual).toEqual(true)
+      expect(user.googleID).not.toBeDefined()
+      expect(user.discordID).toEqual('discord')
     })
   })
 })
