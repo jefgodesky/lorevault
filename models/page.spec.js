16073b9e0e5a510fc083b665c5d2358243197933
Jason Godesky <jefgodesky@gmail.com>
2021-07-18 17:14:56 -0400

    Page.methods.makeUpdate — Add unit tests

--- models/page.spec.js
+++ models/page.spec.js
@@ -30,5 +30,35 @@ describe('Page', () => {
       const actual = await Page.findById(page._id)
       expect(actual.body).toEqual(update.body)
     })
+
+    it('updates the title', async () => {
+      expect.assertions(1)
+      const page = await Page.create(testPageData)
+      const update = JSON.parse(JSON.stringify(testPageData))
+      update.title = 'New Title'
+      await page.makeUpdate(update)
+      const actual = await Page.findById(page._id)
+      expect(actual.title).toEqual(update.title)
+    })
+
+    it('adds a new version', async () => {
+      expect.assertions(1)
+      const page = await Page.create(testPageData)
+      const update = JSON.parse(JSON.stringify(testPageData))
+      update.body = 'This is an update.'
+      await page.makeUpdate(update)
+      const actual = await Page.findById(page._id)
+      expect(actual.versions).toHaveLength(2)
+    })
+
+    it('keeps versions in chronological order', async () => {
+      expect.assertions(1)
+      const page = await Page.create(testPageData)
+      const update = JSON.parse(JSON.stringify(testPageData))
+      update.body = 'This is an update.'
+      await page.makeUpdate(update)
+      const actual = await Page.findById(page._id)
+      expect(actual.versions[1].body).toEqual(update.body)
+    })
   })
 })
