3b361034bdf966e418935780ca8cbd6dc7edd962
Jason Godesky <jefgodesky@gmail.com>
2021-07-30 20:25:36 -0400

    Move GET /profile to users router

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const { Router } = require('express')
 const router = Router()
 
+// GET /profile
+router.get('/profile', async (req, res, next) => {
+  req.viewOpts.title = 'Your Profile'
+  req.viewOpts.connectedGoogle = Boolean(req.user.googleID)
+  req.viewOpts.connectedDiscord = Boolean(req.user.discordID)
+  res.render('profile', req.viewOpts)
+})
+
 // GET /toggle-char-claim-mode
 router.get('/toggle-char-claim-mode', async (req, res, next) => {
   if (req.user) await req.user.toggleCharClaimMode()
