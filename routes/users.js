f8c0e4f5bee1192c83bb8d0eddbba662643b72d6
Jason Godesky <jefgodesky@gmail.com>
2021-08-01 14:11:36 -0400

    Give loremasters the option to turn on loremaster mode

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const Character = require('../models/character')
+const { loremasters } = require('../config')
 const router = Router()
 
 // GET /profile
@@ -8,6 +9,8 @@ router.get('/profile', async (req, res, next) => {
   req.viewOpts.id = req.user._id
   req.viewOpts.connectedGoogle = Boolean(req.user.googleID)
   req.viewOpts.connectedDiscord = Boolean(req.user.discordID)
+  req.viewOpts.loremaster = loremasters.includes(req.user._id.toString())
+  req.viewOpts.perspective = req.user.perspective
   req.viewOpts.characters = await Character.getCharacters(req.user)
   res.render('profile', req.viewOpts)
 })
