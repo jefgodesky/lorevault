38f87ef3644605020b1e5c0865005f86566b9c92
Jason Godesky <jefgodesky@gmail.com>
2021-08-01 11:37:03 -0400

    POST /character/:id/update

--- routes/characters.js
+++ routes/characters.js
@@ -9,10 +9,17 @@ router.get('/:id/update', async (req, res, next) => {
   const char = await Character.findById(req.params.id).populate('page')
   if (!char) return res.redirect('/profile')
   req.viewOpts.char = char
-  console.log(char)
   req.viewOpts.title = `Updating ${char.page.title}`
   req.viewOpts.systems = getSystemsDisplay(rules, char)
   res.render('char-update', req.viewOpts)
+})
+
+// POST /:id/update
+router.post('/:id/update', async (req, res, next) => {
+  let char = await Character.findById(req.params.id)
+  char = Object.assign(char, Character.readForm(req.body))
+  await char.save()
+  res.redirect('/profile')
 })
 
 module.exports = router
