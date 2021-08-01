87838e81cd5f2187b1dcf099b65658a198ca227b
Jason Godesky <jefgodesky@gmail.com>
2021-08-01 11:32:23 -0400

    GET /character/:id/update

--- routes/characters.js
+++ routes/characters.js
@@ -1,13 +1,17 @@
 const { Router } = require('express')
 const Character = require('../models/character')
+const { getSystemsDisplay } = require('../utils')
+const { rules } = require('../config')
 const router = Router()
 
 // GET /:id/update
 router.get('/:id/update', async (req, res, next) => {
-  const char = await Character.findById(req.params.id)
+  const char = await Character.findById(req.params.id).populate('page')
   if (!char) return res.redirect('/profile')
   req.viewOpts.char = char
+  console.log(char)
   req.viewOpts.title = `Updating ${char.page.title}`
+  req.viewOpts.systems = getSystemsDisplay(rules, char)
   res.render('char-update', req.viewOpts)
 })
 
