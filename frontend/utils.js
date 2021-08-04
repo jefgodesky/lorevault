c7cc5537fa61b9bee0b2813ad160badff1f58eed
Jason Godesky <jefgodesky@gmail.com>
2021-08-04 19:52:26 -0400

    New frontend util: create

--- frontend/utils.js
+++ frontend/utils.js
@@ -28,7 +28,28 @@ const select = (selector, all = true) => {
   }
 }
 
+/**
+ * Create a Node.
+ * @param tag {string=} - The name of the tag to use (e.g., `div`, `section`,
+ *   `aside`, `p`, etc.) (Default: `div`).
+ * @param classes {?string[]} - An array of strings to add to the node as
+ *   classes.
+ * @param attrs {?{}} - An object that provides attributes to add to the node,
+ *   expressed as key/value pairs.
+ * @param text {?string} - A string to add as the Node as a text node child.
+ * @returns {Node} - A Node matching the paramaters given.
+ */
+
+const create = (tag = 'div', classes, attrs, text) => {
+  const el = document.createElement(tag)
+  if (classes) el.setAttribute('class', classes.join(' '))
+  if (attrs) Object.keys(attrs).forEach(attr => { el.setAttribute(attr, attrs[attr]) })
+  if (text) el.innerHTML = text
+  return el
+}
+
 export {
   ready,
-  select
+  select,
+  create
 }
--- frontend/utils.spec.js
+++ frontend/utils.spec.js
@@ -5,7 +5,8 @@
 
 import {
   ready,
-  select
+  select,
+  create
 } from './utils'
 
 describe('ready', () => {
@@ -35,5 +36,42 @@ describe('select', () => {
     const actual = select('.test', false)
     expect(actual).toHaveLength(1)
     expect(actual[0].innerHTML).toEqual('Test #1')
+  })
+})
+
+describe('create', () => {
+  it('creates a node', () => {
+    const actual = create()
+    expect(actual.outerHTML).toEqual('<div></div>')
+  })
+
+  it('creates a node of specified tag', () => {
+    const actual = create('p')
+    expect(actual.outerHTML).toEqual('<p></p>')
+  })
+
+  it('creates a node with the given classes', () => {
+    const actual = create(undefined, ['test1', 'test2'])
+    expect(actual.outerHTML).toEqual('<div class="test1 test2"></div>')
+  })
+
+  it('applies other attributes', () => {
+    const actual = create(undefined, undefined, { id: 'test' })
+    expect(actual.outerHTML).toEqual('<div id="test"></div>')
+  })
+
+  it('applies text', () => {
+    const actual = create(undefined, undefined, undefined, 'Test')
+    expect(actual.outerHTML).toEqual('<div>Test</div>')
+  })
+
+  it('applies HTML', () => {
+    const actual = create(undefined, undefined, undefined, '<strong>Test&hellip;</strong>')
+    expect(actual.outerHTML).toEqual('<div><strong>Test…</strong></div>')
+  })
+
+  it('does it all at once', () => {
+    const actual = create('p', ['test1', 'test2'], { id: 'test' }, 'Test')
+    expect(actual.outerHTML).toEqual('<p class="test1 test2" id="test">Test</p>')
   })
 })
