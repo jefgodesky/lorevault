8a17fbbde3af8f2d7431b464c2711c05b123544e
Jason Godesky <jefgodesky@gmail.com>
2021-08-04 20:02:17 -0400

    New frontend util: removeClass

--- frontend/utils.js
+++ frontend/utils.js
@@ -81,10 +81,28 @@ const addClass = (el, ...classes) => {
   })
 }
 
+/**
+ * Removes one or more classes from an element.
+ * @param el {Element} - The element to remove the classes from.
+ * @param classes {string} - The classes to remove from the element.
+ */
+
+const removeClass = (el, ...classes) => {
+  classes.forEach(className => {
+    if (el.classList) {
+      el.classList.remove(className)
+    } else {
+      const curr = el.className.split(' ')
+      el.className = curr.filter(cls => cls !== className).join(' ')
+    }
+  })
+}
+
 export {
   ready,
   select,
   create,
   hasClass,
-  addClass
+  addClass,
+  removeClass
 }
