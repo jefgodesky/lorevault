cc54b4d7c75278e38c06f1ea85a83cefab5adf3f
Jason Godesky <jefgodesky@gmail.com>
2021-08-31 19:45:21 -0400

    UserSchema.methods.disconnect

--- models/user.js
+++ models/user.js
@@ -25,4 +25,21 @@ UserSchema.methods.isLastConnection = function (service) {
   return connections.length === 0
 }
 
+/**
+ * Removes a connection from the user record, but only if it is not the last
+ * connection that the user has left.
+ * @param {string} service - The service to disconnect.
+ * @returns {Promise<boolean>} - A Promise that resolves with `false` if the
+ *   service could not be disconnected (when it's the last remaining connection
+ *   that the user has), or `true` when it's been successfully disconnected and
+ *   the user document has been successfully saved.
+ */
+
+UserSchema.methods.disconnect = async function (service) {
+  if (this.isLastConnection(service)) return false
+  this.login[service] = undefined
+  await this.save()
+  return true
+}
+
 export default model('User', UserSchema)
--- models/user.spec.js
+++ models/user.spec.js
@@ -14,5 +14,22 @@ describe('User', () => {
         expect(user.isLastConnection('google')).to.be.false
       })
     })
+
+    describe('disconnect', () => {
+      it('won\'t remove your last connection', async () => {
+        const user = await User.create({ name: 'Tester', login: { google: 'google' } })
+        const actual = await user.disconnect('google')
+        expect(actual).to.be.false
+        expect(user.login.google).not.to.be.undefined
+      })
+
+      it('will remove a connection if you have another', async () => {
+        const user = await User.create({ name: 'Tester', login: { google: 'google', discord: 'discord' } })
+        const actual = await user.disconnect('google')
+        expect(actual).to.be.true
+        expect(user.login.google).to.be.undefined
+        expect(user.login.discord).not.to.be.undefined
+      })
+    })
   })
 })
