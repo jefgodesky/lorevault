12b064332393cbaeb34895dc26bbd46a362d1b7a
Jason Godesky <jefgodesky@gmail.com>
2021-08-31 17:13:27 -0400

    Nuke it from orbit, it's the only way to be sure.

--- rules/dnd5e/sheet.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  int: { type: Number, label: 'Intelligence Modifier' },
-  arcana: { type: Number, label: 'Arcana Bonus' },
-  history: { type: Number, label: 'History Bonus' },
-  nature: { type: Number, label: 'Nature Bonus' },
-  religion: { type: Number, label: 'Religion Bonus' }
-}
