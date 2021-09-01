a293cd5b67fb1157d938617f325bd6485f65549a
Jason Godesky <jefgodesky@gmail.com>
2021-09-01 17:17:52 -0400

    PageSchema.methods.update

--- models/page.js
+++ models/page.js
@@ -1,3 +1,4 @@
+import smartquotes from 'smartquotes'
 import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
@@ -40,5 +41,25 @@ PageSchema.pre('save', function (next) {
   this.modified = Date.now()
   next()
 })
+
+/**
+ * Add a content update to the page's versions and then save the page.
+ * @param {object} content - An object containing the content to be added as
+ *   the new version.
+ * @param {string} content.title - The title of the new page document version.
+ * @param {string} content.body - The body of the new page document version.
+ * @param {User|Schema.Types.ObjectID|string} editor - Either a User document,
+ *   or the ID of a user. This is the person making the update.
+ * @returns {Promise<void>} - A Promise that resolves once the update has been
+ *   made and the Page document saved to the database.
+ */
+
+PageSchema.methods.update = async function (content, editor) {
+  content.title = smartquotes(content.title)
+  content.body = smartquotes(content.body)
+  this.title = content.title
+  this.versions.push(Object.assign({}, content, { editor: editor._id }))
+  await this.save()
+}
 
 export default model('Page', PageSchema)
--- models/page.spec.js
+++ models/page.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import Page from './page.js'
+import User from './user.js'
 
 describe('Page', () => {
   describe('Presave', () => {
@@ -18,6 +19,53 @@ describe('Page', () => {
         const page = await Page.create({ title: 'Test' })
         await page.save()
         expect(page.modified).to.be.least(start)
+      })
+    })
+  })
+
+  describe('methods', () => {
+    describe('update', () => {
+      it('updates the title', async () => {
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.' }, user)
+        expect(page.title).to.be.equal('After')
+      })
+
+      it('adds a new version', async () => {
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.' }, user)
+        expect(page.versions).to.have.lengthOf(1)
+      })
+
+      it('saves the body of the new version', async () => {
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.' }, user)
+        expect(page.versions[0].body).to.be.equal('This is the updated text.')
+      })
+
+      it('saves a commit message', async () => {
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.', msg: 'This is an update.' }, user)
+        expect(page.versions[0].msg).to.be.equal('This is an update.')
+      })
+
+      it('saves who made the change', async () => {
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.' }, user)
+        expect(page.versions[0].editor).to.be.equal(user._id)
+      })
+
+      it('saves when the change was made', async () => {
+        const start = new Date()
+        const user = await User.create({ name: 'Editor' })
+        const page = await Page.create({ title: 'Before', body: 'This is the original text.' })
+        await page.update({ title: 'After', body: 'This is the updated text.' }, user)
+        expect(page.versions[0].timestamp).to.be.least(start)
       })
     })
   })
