e8262c4db0ec82e006d290e4edbd76ffcc9df1f1
Jason Godesky <jefgodesky@gmail.com>
2021-08-31 20:57:20 -0400

    renderMarkup: Sanitize dangerous HTML

--- transformers/renderMarkup.js
+++ transformers/renderMarkup.js
@@ -1,9 +1,11 @@
+import deepmerge from 'deepmerge'
 import { unified } from 'unified'
 import markdown from 'remark-parse'
 import gfm from 'remark-gfm'
 import sectionize from 'remark-sectionize'
 import remark2rehype from 'remark-rehype'
 import raw from 'rehype-raw'
+import sanitize, { defaultSchema } from 'rehype-sanitize'
 import slugger from 'rehype-slug'
 import linkHeadings from 'rehype-autolink-headings'
 import prettify from 'rehype-format'
@@ -16,12 +18,19 @@ import rehypeStringify from 'rehype-stringify'
  */
 
 const renderMarkup = async str => {
+  const schema = deepmerge(defaultSchema, {
+    attributes: {
+      '*': ['class']
+    },
+    tagNames: ['section', 'aside', 'figure', 'figcaption']
+  })
   const render = await unified()
     .use(markdown)
     .use(gfm)
     .use(sectionize)
     .use(remark2rehype, { allowDangerousHtml: true })
     .use(raw)
+    .use(sanitize, schema)
     .use(slugger)
     .use(linkHeadings)
     .use(prettify)
--- transformers/renderMarkup.spec.js
+++ transformers/renderMarkup.spec.js
@@ -12,6 +12,11 @@ describe('renderMarkup', () => {
     expect(actual).to.be.equal('\n<p><strong>Hello, world!</strong></p>\n')
   })
 
+  it('doesn\'t allow *all* HTML', async () => {
+    const actual = await renderMarkup('<strong>Hello, world!</strong> <script>Inside a script tag</script>')
+    expect(actual).to.be.equal('\n<p><strong>Hello, world!</strong></p>\n')
+  })
+
   it('handles tables', async () => {
     const actual = await renderMarkup('A | B | C\n--- | --- | ---\n1|2|3')
     expect(actual).to.be.equal('\n<table>\n  <thead>\n    <tr>\n      <th>A</th>\n      <th>B</th>\n      <th>C</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <td>1</td>\n      <td>2</td>\n      <td>3</td>\n    </tr>\n  </tbody>\n</table>\n')
