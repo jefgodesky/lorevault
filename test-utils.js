b540852a42075ecc1ea52606eba9b4e1f7b6b676
Jason Godesky <jefgodesky@gmail.com>
2021-09-01 20:39:22 -0400

    Add new test util method: createTestDocs

--- test-utils.js
+++ test-utils.js
@@ -1,4 +1,20 @@
 /**
+ * Set up standard test documents in the database.
+ * @param {function} model - The Mongoose model function.
+ * @returns {Promise<{user: User, other: User, page: Page}>} - A Promise that
+ *   resolves with test documents that have been created in the database.
+ */
+
+const createTestDocs = async model => {
+  const Page = model('Page')
+  const User = model('User')
+  const user = await User.create({ name: 'Tester 1' })
+  const other = await User.create({ name: 'Tester 2' })
+  const page = await Page.create({ title: 'Test Page', body: 'This is the original text.' }, user)
+  return { user, other, page }
+}
+
+/**
  * Find a string not yet in use by the given object as a property name.
  * @param {object} obj - The object we're examining.
  * @returns {string} - A string that the object is not yet using as the name of
@@ -15,5 +31,6 @@ const codenamer = obj => {
 }
 
 export {
+  createTestDocs,
   codenamer
 }
--- test-utils.spec.js
+++ test-utils.spec.js
@@ -1,6 +1,40 @@
 import { expect } from 'chai'
+import mongoose from 'mongoose'
+const { model } = mongoose
 import { pickRandomNum } from './utils.js'
-import { codenamer } from './test-utils.js'
+import { createTestDocs, codenamer } from './test-utils.js'
+
+describe('createTestDocs', () => {
+  it('creates a user', async () => {
+    const { user } = await createTestDocs(model)
+    expect(user.constructor.modelName).to.be.equal('User')
+  })
+
+  it('creates a second user', async () => {
+    const { other } = await createTestDocs(model)
+    expect(other.constructor.modelName).to.be.equal('User')
+  })
+
+  it('creates two distinct users', async () => {
+    const { user, other } = await createTestDocs(model)
+    expect(user._id).not.to.be.equal(other._id)
+  })
+
+  it('creates a page', async () => {
+    const { page } = await createTestDocs(model)
+    expect(page.constructor.modelName).to.be.equal('Page')
+  })
+
+  it('sets the test page\'s title', async () => {
+    const { page } = await createTestDocs(model)
+    expect(page.title).to.be.equal('Test Page')
+  })
+
+  it('creates an initial version of the test page', async () => {
+    const { page } = await createTestDocs(model)
+    expect(page.versions).to.have.lengthOf(1)
+  })
+})
 
 describe('codenamer', () => {
   it('returns a new valid key each time', () => {
