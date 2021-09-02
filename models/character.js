e8ab5f444249ed444248d7bf82ade0e36e77b986
Jason Godesky <jefgodesky@gmail.com>
2021-09-02 19:12:15 -0400

    CharacterSchema.statics.create

--- models/character.js
+++ models/character.js
@@ -37,4 +37,38 @@ const CharacterSchema = new Schema(await addGames({
   }
 }))
 
+/**
+ * Create a new character.
+ * @param {Page|Schema.Types.ObjectId|string} page - The character's page
+ *   document (or its ID, or the string representation of its ID).
+ * @param {User|Schema.Types.ObjectId|string} player - The user who plays this
+ *   character (or the user's ID, or the string representing hens ID).
+ * @param {{}} stats - An object with properties defining the values for the
+ *   character's statistics in the games defined in the configuration. For
+ *   example, `dnd5e-int` should provide the character's `int` statistic as
+ *   defined by the game `dnd5e`.
+ * @returns {Promise<Character>} - The Character once it has been created and
+ *   saved to the database.
+ */
+
+CharacterSchema.statics.create = async (page, player, stats) => {
+  const Character = model('Character')
+  const char = new Character()
+  char.page = page?._id || page
+  char.player = player?._id || player
+
+  for (const game of config.games) {
+    const { info } = await import(`../games/${game}/${game}.js`)
+    for (const stat of info.sheet) {
+      const s = stats ? stats[`${game}-${stat.id}`] : null
+      const isNum = stat.type === Number
+      const isValid = !s ? false : isNum ? !isNaN(parseInt(s)) : true
+      if (isValid) char[game][stat.id] = isNum ? parseInt(s) : s
+    }
+  }
+
+  await char.save()
+  return char
+}
+
 export default model('Character', CharacterSchema)
--- models/character.spec.js
+++ models/character.spec.js
@@ -1,4 +1,9 @@
 import { expect } from 'chai'
+
+import mongoose from 'mongoose'
+const { model } = mongoose
+
+import { createTestDocs } from '../test-utils.js'
 import Character from './character.js'
 
 describe('Character', () => {
@@ -7,6 +12,33 @@ describe('Character', () => {
       const char = new Character()
       const { int, arcana, history, nature, religion } = char.dnd5e
       expect([int, arcana, history, nature, religion]).to.be.eql([0, 0, 0, 0, 0])
+    })
+  })
+
+  describe('statics', () => {
+    describe('create', () => {
+      it('it creates a new character', async () => {
+        const char = await Character.create()
+        expect(char.constructor.modelName).to.be.equal('Character')
+      })
+
+      it('sets the character\'s page', async () => {
+        const { page } = await createTestDocs(model)
+        const char = await Character.create(page)
+        expect(char.page).to.be.equal(page._id)
+      })
+
+      it('sets the character\'s player', async () => {
+        const { page, user } = await createTestDocs(model)
+        const char = await Character.create(page, user)
+        expect(char.player).to.be.equal(user._id)
+      })
+
+      it('sets the character\'s statistics', async () => {
+        const { page, user } = await createTestDocs(model)
+        const char = await Character.create(page, user, { 'dnd5e-int': '3' })
+        expect(char.dnd5e.int).to.be.equal(3)
+      })
     })
   })
 })
