083efd43bbeb2b4bbe28ce10d20f8f62b65a9f4a
Jason Godesky <jefgodesky@gmail.com>
2021-09-03 17:53:52 -0400

    PageSchema.methods.write

--- models/page.js
+++ models/page.js
@@ -220,6 +220,61 @@ PageSchema.methods.knows = function (char, codename) {
 }
 
 /**
+ * Write a version of the body according to the parameters provided.
+ * @param {object} params - An object defining the parameters of how the body
+ *   should be written.
+ * @param {Character|string} [params.pov = 'Anonymous'] - The point of view
+ *   from which the body should be written. If this is the string `Loremaster,`
+ *   then all secrets are shown. If this is the string `Anonymous` (which it is
+ *   by default), then no secrets are shown. If given a Character, only those
+ *   secrets which are known to that character are shown.
+ * @param {string} [params.mode = 'reading'] - The mode in which we are writing
+ *   the body from the given version. In `full` mode, we fully expand all
+ *   secrets. This is typically used in preparing the version to be saved to
+ *   the database. In `editing` mode, codenames are used to create placeholders
+ *   for where secrets appear in the text for points of view that are not privy
+ *   to those secrets. In `reading` mode, no indication is made of any secrets
+ *   that the point of view is not privy to. Acceptable strings for this
+ *   parameter are `full`, `editing`, or `reading`. (Default: `reading`)
+ * @param {string} params.str - If provided, the method will work on this
+ *   string instead of the body of any exisitng version of the page. This
+ *   overrides `params.version`.
+ * @param {Version} [params.version = this.getCurr()] - The version from which
+ *   we take the body that we're going to write.
+ * @returns {string} - The body written according to the parameters provided.
+ */
+
+PageSchema.methods.write = function (params = {}) {
+  const { pov = 'Anonymous', mode = 'reading', version = this.getCurr() } = params
+  const secrets = this.getSecrets(pov)
+  let str = params.str || version.body
+
+  const full = mode.toLowerCase() === 'full'
+  const editing = !full && mode.toLowerCase() === 'editing'
+  const reading = !full && !editing
+
+  for (const secret of secrets) {
+    const txt = full || (editing && secret.known)
+      ? `||::${secret.codename}:: ${secret.content}||`
+      : editing && !secret.known
+        ? `||::${secret.codename}::||`
+        : reading && secret.known
+          ? secret.content
+          : ''
+
+    const regex = new RegExp(`\\|\\|::${secret.codename}::\\s*?.*?\\|\\|`, 'gm')
+    const match = str.match(regex)
+    if (match) {
+      str = str.replace(match, txt)
+    } else if ((full || editing) && !secret.known) {
+      str += `\n\n${txt}`
+    }
+  }
+
+  return str.trim()
+}
+
+/**
  * Add a content update to the page's versions and then save the page.
  * @param {object} content - An object containing the content to be added as
  *   the new version.
