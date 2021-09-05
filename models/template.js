4fa3c455b106e6de2e317ac3d24e10445b1b8b20
Jason Godesky <jefgodesky@gmail.com>
2021-09-05 17:13:07 -0400

    Template.parseParams

--- models/template.js
+++ models/template.js
@@ -1,6 +1,40 @@
+import { saveBlocks, restoreBlocks } from '../utils.js'
+
 class Template {
   constructor (arg) {
     this.name = arg.name
+  }
+
+  /**
+   * Parse a string representing a template's parameters into an object.
+   * @param {string} str - A string representing the template's parameters.
+   * @return {{ordered: string[], named: {}}} - An object representing the
+   *   template's parameters, with the following properties:
+   *     `ordered`   The template's ordered parameters. This is an array of
+   *                 strings. Each parameter is identified by its index within
+   *                 the array.
+   *     `named`     The template's named parameters. This is an object that
+   *                 provides a dictionary of key/value pairs for each of the
+   *                 template's named parameters.
+   */
+
+  static parseParams (str) {
+    const params = { ordered: [], named: {} }
+    const brackets = saveBlocks(str, /\[\[(.*?)\]\]/im, 'BRACKET')
+    const elems = brackets.str.split('|').map(el => el.trim())
+    for (const elem of elems) {
+      const pair = elem.split('=').map(el => el.trim())
+      const val = restoreBlocks(pair[pair.length - 1], brackets.blocks)
+      const int = parseInt(pair[0])
+      if (pair.length < 2) {
+        params.ordered.push(val)
+      } else if (isNaN(int)) {
+        params.named[pair[0]] = val
+      } else {
+        params.ordered[int - 1] = val
+      }
+    }
+    return params
   }
 }
 
--- models/template.spec.js
+++ models/template.spec.js
@@ -8,4 +8,26 @@ describe('Template', () => {
       expect(actual.name).to.be.equal('Test')
     })
   })
+
+  describe('parseParams', () => {
+    it('can parse ordered parameters', () => {
+      const actual = Template.parseParams('hello|world')
+      expect(actual.ordered).to.be.eql(['hello', 'world'])
+    })
+
+    it('can parse explicitly ordered parameters', () => {
+      const actual = Template.parseParams('  2=world\n  |1=hello')
+      expect(actual.ordered).to.be.eql(['hello', 'world'])
+    })
+
+    it('can parse named parameters', () => {
+      const actual = Template.parseParams('  greeting=hello\n  |subject=world')
+      expect(actual.named).to.be.eql({ greeting: 'hello', subject: 'world' })
+    })
+
+    it('can handle links', () => {
+      const actual = Template.parseParams('link=[[Test Page | Alias]]')
+      expect(actual.named).to.be.eql({ link: '[[Test Page | Alias]]' })
+    })
+  })
 })
