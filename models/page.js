1c005db5890c12f6a2dedca53cfeac889a5be554
Jason Godesky <jefgodesky@gmail.com>
2021-09-11 17:58:28 -0400

    Fix documentation

--- models/page.js
+++ models/page.js
@@ -499,7 +499,8 @@ PageSchema.methods.revealToName = async function (name, codename = null) {
  * given `codename`.
  * @param {Character|Schema.Types.ObjectId|string} char - The character who
  *   might (or might not) know the secret (or hens ID, or the string
- *   representation of hens ID).
+ *   representation of hens ID), or the string 'Anonymous' for an anonymous
+ *   user, or the string 'Loremaster' for a loremaster.
  * @param {string} codename - The codename identifying the secret. If you
  *   provide a falsy value (e.g., `false` or `null`) and the page itself is a
  *   secret, then the secret the method reports on is the existence of the page
