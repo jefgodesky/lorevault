7d40677a5432f3bd55527577a24ed060deb0d13f
Jason Godesky <jefgodesky@gmail.com>
2021-10-02 10:23:11 -0400

    Secret.parse

--- models/secret.js
+++ models/secret.js
@@ -7,6 +7,33 @@ class Secret {
     this.knowers = knowers
     this.checked = checked
   }
+
+  /**
+   * Parse <secret> tags from a string.
+   * @param {string} str - The string to be parsed.
+   * @param {function} codenamer - A function that produces codenames for those
+   *   secrets that haven't set one.
+   * @returns {Secret[]} - An array of secrets parsed from the given string.
+   */
+  
+  static parse (str, codenamer) {
+    const tags = str.match(/<secret(.*?)>(.*?)<\/secret>/gmi)
+    if (!tags) return []
+    const secrets = []
+    for (const tag of tags) {
+      const elems = tag.match(/<secret(.*?)>(.*?)<\/secret>/i)
+      if (!elems || elems.length < 3) continue
+      const attrMatch = elems[1] !== '' ? elems[1].match(/([A-Za-z]*?)=["“](.*?)["”]/gmi) : []
+      const attrs = {}
+      for (const attr of attrMatch) {
+        const pair = attr.split('=')
+        if (!pair || pair.length < 2) continue
+        attrs[pair[0]] = pair[1].substring(1, pair[1].length - 1)
+      }
+      secrets.push(new Secret(Object.assign({}, attrs, { content: elems[2] }), codenamer))
+    }
+    return secrets
+  }
 }
 
 export default Secret
--- models/secret.spec.js
+++ models/secret.spec.js
@@ -2,70 +2,96 @@ import { expect } from 'chai'
 import Secret from './secret.js'
 
 describe('Secret', () => {
-  describe('Constructor', () => {
-    let num = 0
-    const codenamer = () => {
-      num++
-      return `secret${num.toString().padStart(3, '0')}`
-    }
+  let num = 0
+  const codenamer = () => {
+    num++
+    return `secret${num.toString().padStart(3, '0')}`
+  }
 
+  beforeEach(() => { num = 0 })
+
+  describe('Constructor', () => {
     it('takes a codename', () => {
       const { codename } = new Secret({ codename: 'Wombat' })
       expect(codename).to.be.equal('Wombat')
     })
 
     it('takes a default codenaming function', () => {
-      num = 0
       const { codename } = new Secret({}, codenamer)
       expect(codename).to.be.equal('secret001')
     })
 
     it('takes content', () => {
-      num = 0
       const { content } = new Secret({ content: 'Hello world!' }, codenamer)
       expect(content).to.be.equal('Hello world!')
     })
 
     it('defaults content to a blank string', () => {
-      num = 0
       const { content } = new Secret({}, codenamer)
       expect(content).to.be.equal('')
     })
 
     it('takes conditions', () => {
-      num = 0
       const { conditions } = new Secret({ conditions: 'this and that' }, codenamer)
       expect(conditions).to.be.equal('this and that')
     })
 
     it('defaults conditions to a blank string', () => {
-      num = 0
       const { conditions } = new Secret({}, codenamer)
       expect(conditions).to.be.equal('')
     })
 
     it('takes knowers', () => {
-      num = 0
       const { knowers } = new Secret({ knowers: [1, 2, 3] }, codenamer)
       expect(knowers).to.be.eql([1, 2, 3])
     })
 
     it('defaults knowers to an empty array', () => {
-      num = 0
       const { knowers } = new Secret({}, codenamer)
       expect(knowers).to.be.eql([])
     })
 
     it('takes checked', () => {
-      num = 0
       const { checked } = new Secret({ checked: [1, 2, 3] }, codenamer)
       expect(checked).to.be.eql([1, 2, 3])
     })
 
     it('defaults checked to an empty array', () => {
-      num = 0
       const { checked } = new Secret({}, codenamer)
       expect(checked).to.be.eql([])
+    })
+  })
+
+  describe('Statics', () => {
+    describe('parse', () => {
+      it('parses a secret', () => {
+        const str = 'A paragraph before.\n\nSome text before <secret>the terrible secret</secret>. Then some more text.\n\nAnd a paragraph below.'
+        const actual = Secret.parse(str, codenamer)
+        expect(actual).to.have.lengthOf(1)
+        expect(actual[0].codename).to.be.equal('secret001')
+        expect(actual[0].content).to.be.equal('the terrible secret')
+        expect(actual[0].conditions).to.be.equal('')
+        expect(actual[0].knowers).to.be.eql([])
+        expect(actual[0].checked).to.be.eql([])
+      })
+
+      it('parses many secrets', () => {
+        const str = '<secret>1</secret> <secret>2</secret> <secret>3</secret>'
+        const actual = Secret.parse(str, codenamer)
+        expect(actual).to.have.lengthOf(3)
+      })
+
+      it('parses codenames', () => {
+        const str = '<secret codename="Wombat">This is a secret</secret>'
+        const actual = Secret.parse(str, codenamer)
+        expect(actual[0].codename).to.be.equal('Wombat')
+      })
+
+      it('parses conditions', () => {
+        const str = '<secret conditions="[Volaarun-speaker]">This is a secret</secret>'
+        const actual = Secret.parse(str, codenamer)
+        expect(actual[0].conditions).to.be.equal('[Volaarun-speaker]')
+      })
     })
   })
 })
