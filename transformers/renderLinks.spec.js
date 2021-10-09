843ddc86d5bd7ea846d2846dd7a71265ffe6e129
Jason Godesky <jefgodesky@gmail.com>
2021-10-09 11:45:19 -0400

    renderLinks: Never actually used the secrets argument

--- transformers/renderLinks.spec.js
+++ transformers/renderLinks.spec.js
@@ -10,57 +10,56 @@ import renderLinks from './renderLinks.js'
 describe('renderLinks', () => {
   it('renders links', async () => {
     const { user } = await createTestDocs(model)
-    const { str } = await renderLinks('[[Test Page|Hello!]]', {}, user)
+    const { str } = await renderLinks('[[Test Page|Hello!]]', user)
     expect(str).to.be.equal('<a href="/test-page" title="Test Page">Hello!</a>')
   })
 
   it('renders links for new pages', async () => {
     const { user } = await createTestDocs(model)
-    const { str } = await renderLinks('[[New Page|Hello!]]', {}, user)
+    const { str } = await renderLinks('[[New Page|Hello!]]', user)
     expect(str).to.be.equal('<a href="/create?title=New%20Page" class="new">Hello!</a>')
   })
 
   it('returns the pages that are linked', async () => {
     const { page, user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[Test Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[Test Page|Hello!]]', user)
     expect(links[0].page._id.toString()).to.be.eql(page._id.toString())
   })
 
   it('returns null when linking to a page that doesn\'t exist yet', async () => {
     const { user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[New Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[New Page|Hello!]]', user)
     expect(links[0].page).to.be.null
   })
 
   it('returns the titles of the pages that are linked', async () => {
     const { page, user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[Test Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[Test Page|Hello!]]', user)
     expect(links[0].title).to.be.eql(page.title)
   })
 
   it('returns titles from link when pages don\'t exist', async () => {
     const { user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[New Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[New Page|Hello!]]', user)
     expect(links[0].title).to.be.eql('New Page')
   })
 
   it('returns the text used for the link', async () => {
     const { user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[Test Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[Test Page|Hello!]]', user)
     expect(links[0].text).to.be.eql('Hello!')
   })
 
   it('returns false if the link is not part of a secret', async () => {
     const { user } = await createTestDocs(model)
-    const { links } = await renderLinks('[[Test Page|Hello!]]', {}, user)
+    const { links } = await renderLinks('[[Test Page|Hello!]]', user)
     expect(links[0].secret).to.be.false
   })
 
   it('returns the codename if the link is part of a secret', async () => {
     const { user } = await createTestDocs(model)
     const str = '<secret codename="Wombat">[[Test Page|Hello!]]</secret>>'
-    const secrets = Secret.parse(str, codenamer)
-    const { links } = await renderLinks(str, secrets, user)
+    const { links } = await renderLinks(str, user)
     expect(links[0].secret).to.be.equal('Wombat')
   })
 })
